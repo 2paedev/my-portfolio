5a410fccc595f8b231d1edbcde0e3ecc7561e3d3
2paedev <2paedev@gmail.com>
2020-03-14 09:10:43 +0100

    scaffolding

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import Vue from 'vue'
+import App from './App.vue'
+import './assets/styles/main.scss'
+
+Vue.config.productionTip = false
+
+new Vue({
+  created() {
+    AOS.init()
+  },
+  render: h => h(App)
+}).$mount('#app')
